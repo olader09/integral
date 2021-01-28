ece614c19a49f574cf8729ebf7c9012ee7ed0b72
Igor Kondrashov <reid09@mail.ru>
2021-01-28 11:18:20 +0300

    validates price

--- api/app/models/dish.rb
+++ api/app/models/dish.rb
@@ -1,6 +1,8 @@
 class Dish < ApplicationRecord
   after_create :init_zindex
 
+  validates :price, presence: true
+
   mount_uploader :picture, DishUploader
   mount_base64_uploader :picture, DishUploader
 
