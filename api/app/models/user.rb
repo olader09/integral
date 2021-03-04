f68475fb2da4e50ffa198c3ac9ab99acca6a0648
Igor Kondrashov <reid09@mail.ru>
2021-03-04 21:02:18 +0300

    fix

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   extend Enumerize
   
   validates :phone_number, presence: true, uniqueness: { case_sensitive: true }
-  has_secure_password
+  has_secure_password validations: false
   has_many :orders
 
   enumerize :role, in: %i[user], default: :user, predicates: true, scope: :shallow
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 
   def generate_password(phone_number)
     p new_password = rand(0000..9999).to_s.rjust(4, '0')
-    password = new_password
+    self.password = new_password
     save
     # sms_service = SmsPasswordService.new(phone_number, new_password)
     # response = sms_service.send_request_sms_password
