e1cf5620e03c33d5970a49e1d0f3e993074d59d2
Igor <reid09@mail.ru>
2021-04-07 20:16:53 +0300

    dependent destroy orders

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   
   validates :phone_number, presence: true, uniqueness: { case_sensitive: true }
   has_secure_password validations: false
-  has_many :orders
+  has_many :orders, dependent: :destroy
 
   enumerize :role, in: %i[user], default: :user, predicates: true, scope: :shallow
 
